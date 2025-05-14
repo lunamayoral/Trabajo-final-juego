0181f28ced35a8c2b5c3104e0c65b6b232e44d5c
lunamayoral <lunabookblog@gmail.com>
2025-05-14 15:54:44 +0200

    cambio tamaño

--- src/main/java/es/uah/matcomp/mp/teoria/gui/mvc/javafx/trabajofinaljuego/Facciones.java
+++ src/main/java/es/uah/matcomp/mp/teoria/gui/mvc/javafx/trabajofinaljuego/Facciones.java
@@ -12,7 +12,7 @@ public class Facciones extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("facciones-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 500, 400);
+        Scene scene = new Scene(fxmlLoader.load(), 380, 360);
         stage.setTitle("Facciones");
 
         GridPane grid = new GridPane();
