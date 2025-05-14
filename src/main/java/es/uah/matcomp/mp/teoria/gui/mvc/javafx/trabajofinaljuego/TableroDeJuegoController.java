bcfcdcad7072f7c6bebbf7dcba2aea4c2d6e29cb
lunamayoral <lunabookblog@gmail.com>
2025-05-14 13:04:57 +0200

    tablero variable

--- src/main/java/es/uah/matcomp/mp/teoria/gui/mvc/javafx/trabajofinaljuego/TableroDeJuegoController.java
+++ src/main/java/es/uah/matcomp/mp/teoria/gui/mvc/javafx/trabajofinaljuego/TableroDeJuegoController.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -12,6 +13,26 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 public class TableroDeJuegoController implements Initializable {
+    @FXML
+    private GridPane tableroGrid; // GridPane definido en tablero.fxml
+    public void inicializarTablero(int altura, int anchura) {
+        // Limpiar tablero previo
+        tableroGrid.getChildren().clear();
+        tableroGrid.setHgap(5);
+        tableroGrid.setVgap(5);
+        for (int fila = 0; fila < altura; fila++) {
+            for (int col = 0; col < anchura; col++) {
+                Button celda = new Button();
+                celda.setPrefSize(40, 40);
+                celda.setStyle("-fx-background-color: lightgray; -fx-border-color: black;");
+                // Acción ejemplo: cambiar color al pulsar
+                celda.setOnAction(e -> {
+                    celda.setStyle("-fx-background-color: deepskyblue; -fx-border-color: black;");
+                });
+                tableroGrid.add(celda, col, fila);
+            }
+        }
+    }
 
 
 
--- src/main/java/es/uah/matcomp/mp/teoria/gui/mvc/javafx/trabajofinaljuego/TamañoTableroController.java
+++ src/main/java/es/uah/matcomp/mp/teoria/gui/mvc/javafx/trabajofinaljuego/TamañoTableroController.java
@@ -1,9 +1,16 @@
 package es.uah.matcomp.mp.teoria.gui.mvc.javafx.trabajofinaljuego;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -14,15 +21,38 @@ public class TamañoTableroController implements Initializable {
     private Label altura;
     @FXML
     private Label anchura;
-
-    private Label generarTablero;
     @FXML
-    private void generarTableroButtonClick() {
-        generarTablero.setText("Generar Tablero");
+    private TextField alturaField;  // Para que el jugador ponga la altura (filas)
+    @FXML
+    private TextField anchuraField; // Para que el jugador ponga la anchura (columnas)
+
+    @FXML
+    private void GenerarTablero() {
+        String alturaText = alturaField.getText();
+        String anchuraText = anchuraField.getText();
+        try {
+            int altura = Integer.parseInt(alturaText);
+            int anchura = Integer.parseInt(anchuraText);
+            if (altura <= 0 || anchura <= 0) {
+                System.out.println("Introduce valores positivos para altura y anchura.");
+                return;
+            }
+            // Cargar la nueva pantalla con el tablero
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("tablerodejuego-view.fxml"));
+            Parent root = loader.load();
+            TableroDeJuegoController controladorTablero = loader.getController();
+            controladorTablero.inicializarTablero(altura, anchura);
+            Stage stage = (Stage) alturaField.getScene().getWindow();
+            stage.setScene(new Scene(root));
+            stage.setTitle("Tablero " + altura + " x " + anchura);
+            stage.show();
+        } catch (NumberFormatException e) {
+            System.out.println("Introduce números válidos en altura y anchura.");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
-    public void initialize(URL url, ResourceBundle resourceBundle) {
-
-    }
+    public void initialize(URL url, ResourceBundle resourceBundle) {}
 
 }
