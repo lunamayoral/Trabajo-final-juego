92157c0350bcc951738f93403ff2b4b070ea41d7
irenemartingarcia <irene.martingarcia@edu.uah.es>
2025-05-14 12:02:27 +0200

    actualizacion

--- src/main/java/es/uah/matcomp/mp/teoria/gui/mvc/javafx/trabajofinaljuego/InstruccinesController.java
+++ src/main/java/es/uah/matcomp/mp/teoria/gui/mvc/javafx/trabajofinaljuego/InstruccinesController.java
@@ -1,5 +1,13 @@
 package es.uah.matcomp.mp.teoria.gui.mvc.javafx.trabajofinaljuego;
 
-public class InstruccinesController {
+import javafx.fxml.Initializable;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class InstruccinesController implements Initializable {
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+    }
 }
--- src/main/java/es/uah/matcomp/mp/teoria/gui/mvc/javafx/trabajofinaljuego/PantallaPrincipalController.java
+++ src/main/java/es/uah/matcomp/mp/teoria/gui/mvc/javafx/trabajofinaljuego/PantallaPrincipalController.java
@@ -1,4 +1,34 @@
 package es.uah.matcomp.mp.teoria.gui.mvc.javafx.trabajofinaljuego;
 
-public class PantallaPrincipalController {
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class PantallaPrincipalController implements Initializable {
+
+    @FXML
+    public void Instrucciones(){
+        //Conexion a la siguiente ventana: instrucciones
+        Stage stage = new Stage();
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Instrucciones-view.fxml"));
+        //para conectar a la venta siguiente necesito el fxml de la ventana siguiente
+        try {
+            Scene scene = new Scene(fxmlLoader.load(), 500, 500);
+            stage.setTitle("Instrucciones");
+            stage.setScene(scene);
+            stage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+    }
 }
+
+
