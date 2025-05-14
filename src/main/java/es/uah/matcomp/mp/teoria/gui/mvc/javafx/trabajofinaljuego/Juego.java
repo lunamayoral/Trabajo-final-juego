288a441782a0c11b51f8fc9725f26de4dc3ce06a
lunamayoral <lunabookblog@gmail.com>
2025-05-14 16:54:56 +0200

    clase donde se crea el juego vacia

---
+++ src/main/java/es/uah/matcomp/mp/teoria/gui/mvc/javafx/trabajofinaljuego/Juego.java
@@ -0,0 +1,24 @@
+package es.uah.matcomp.mp.teoria.gui.mvc.javafx.trabajofinaljuego;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Juego extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Juego-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        stage.setTitle("Juego!");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+
+}
---
+++ src/main/java/es/uah/matcomp/mp/teoria/gui/mvc/javafx/trabajofinaljuego/JuegoController.java
@@ -0,0 +1,10 @@
+package es.uah.matcomp.mp.teoria.gui.mvc.javafx.trabajofinaljuego;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class JuegoController implements Initializable {
+    public void initialize(URL url, ResourceBundle resourceBundle) {}
+}
