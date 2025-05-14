63b47c586212ccecf17b409b6347bfd227b1caef
Samug <samuel.garciabarrio@edu.uah.es>
2025-05-14 12:04:41 +0200

    Pantalla Guardar

--- src/main/java/es/uah/matcomp/mp/teoria/gui/mvc/javafx/trabajofinaljuego/GuardarController.java
+++ src/main/java/es/uah/matcomp/mp/teoria/gui/mvc/javafx/trabajofinaljuego/GuardarController.java
@@ -5,14 +5,4 @@ import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 
 public class GuardarController {
-    @FXML
-    private PasswordField Id;
-
-    @FXML
-    private Label Partida;
-
-    @FXML
-    protected void onAcceptBttClick(){
-        Partida.setText("Su código de partida es: "+Id.getText()+", asegúrese de no perderlo");
-    }
 }
