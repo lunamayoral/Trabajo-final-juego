8f335c39bb1a96048612ee2afafd45f2764964f2
DaniOrtegaR13 <d.ortegar@edu.uah.es>
2025-05-26 13:42:21 +0200

    Reapply "boton de salir y personajes"
    This reverts commit 62785fce5f241e8b48445862625f611d8003f2c9.

--- src/main/java/es/uah/matcomp/mp/teoria/gui/mvc/javafx/trabajofinaljuego/Juego.java
+++ src/main/java/es/uah/matcomp/mp/teoria/gui/mvc/javafx/trabajofinaljuego/Juego.java
@@ -4,10 +4,10 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
 import java.io.IOException;
 
 public class Juego extends Application {
+
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Juego-view.fxml"));
@@ -20,5 +20,4 @@ public class Juego extends Application {
     public static void main(String[] args) {
         launch();
     }
-
 }
